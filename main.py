cf4bdea6dca3dace3e7785217235b00136d5ef7e
Trung Nguyen <136333659+trungnguyen2411@users.noreply.github.com>
2025-08-12 11:09:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+        case 'show' | 'display':
+            for index, item in enumerate(todos):
+                item = item.title()
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete:"))
+            todos.pop(number - 1)
+        case 'exit':
+            break
+
+print("Bye!")
+
+
+
