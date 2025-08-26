cf340146b04f4cc29cb6ddea7e2fd2b9c68143fb
trungnguyen2411 <https://github.com/trungnguyen2411>
2025-08-26 13:00:55 +0700

    changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + '\n')
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
