240065eeedb5ab46342c47537f932d22e9ce952e
trungnguyen2411 <https://github.com/trungnguyen2411>
2025-09-20 19:20:06 +0700

    changes

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
     """
@@ -7,12 +10,11 @@ def get_todos(filepath="todos.txt"):
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
-print(__name__)
 
 if __name__ == "__main__":
     print("Hello")
